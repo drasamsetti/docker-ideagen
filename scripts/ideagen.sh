b19b2b04862155921b470d1d5face6e946f4aceb
DURGA PRASAD R <drasamsetti@csc.com>
2015-05-13 19:37:58 +0530

    port 443 is added

--- scripts/ideagen.sh
+++ scripts/ideagen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name ideagen -d -p 80:80 -p 3306:3306 -v `pwd`/../db:/tmp cscdock/ideagen
+sudo docker run -d -p 80:80 -p 443:443 -p 3306:3306 -v `pwd`/../db:/tmp --name ideagen drasamsetti/ideagen
